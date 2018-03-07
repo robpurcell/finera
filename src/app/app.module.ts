d32caf914445002c2c8b1c2f58dbfada91148ad0
Rob Purcell <rob.purcell@gmail.com>
2018-03-07 06:30:32 +0000

    --WIP-- Angular tutorial - new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
 import {NgModule} from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     NgbModule.forRoot()
   ],
   providers: [],
